581d5485f842fadcc06535d3a23596de343527c1
ahmtakin <aa.akin57@gmail.com>
2024-12-26 19:50:06 +0300

    Add favorite page

---
+++ Interfaces/IFavoriteRecipeRepository.cs
@@ -0,0 +1,11 @@
+﻿using _8bits_app_api.Models;
+
+namespace _8bits_app_api.Interfaces
+{
+    public interface IFavoriteRecipeRepository
+    {
+        Task<FavoriteRecipe> AddFavoriteAsync(FavoriteRecipe favoriteRecipe);
+        Task<bool> RemoveFavoriteAsync(int userId, int recipeId);
+        Task<IEnumerable<FavoriteRecipe>> GetFavoritesByUserIdAsync(int userId);
+    }
+}
