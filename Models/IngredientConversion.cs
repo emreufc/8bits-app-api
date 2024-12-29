ba4a6b9a10d377586e535c6b2674762e9b077187
itu-itis21-aydil20 <aydil20@itu.edu.tr>
2024-12-29 17:27:34 +0300

    Push

---
+++ Models/IngredientConversion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8bits_app_api.Models;
+
+public partial class IngredientConversion
+{
+    public int IngredientId { get; set; }
+
+    public string IngredientName { get; set; } = null!;
+
+    public int QuantityTypeId { get; set; }
+
+    public string QuantityTypeDesc { get; set; } = null!;
+
+    public double? ConversionToMl { get; set; }
+
+    public double? ConversionToGrams { get; set; }
+}
