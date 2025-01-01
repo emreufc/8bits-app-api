0a77d6908d19b4e9f8b75cfe443be4d2ed053f2f
ahmtakin <aa.akin57@gmail.com>
2025-01-01 23:14:35 +0300

    RecipeImage

---
+++ Models/RecipeImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8bits_app_api.Models;
+
+public partial class RecipeImage
+{
+    public int RecipeImageId { get; set; }
+
+    public int RecipeId { get; set; }
+
+    public string ImagePath { get; set; } = null!;
+}
