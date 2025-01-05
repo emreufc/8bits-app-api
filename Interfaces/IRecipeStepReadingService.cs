73fe3b0e1a484765650035d2367e452db2a69111
ahmtakin <aa.akin57@gmail.com>
2025-01-05 18:03:42 +0300

    Add recipe step control for admin user

--- Interfaces/IRecipeStepReadingService.cs
+++ Interfaces/IRecipeStepReadingService.cs
@@ -7,6 +7,9 @@ namespace _8bits_app_api.Services
         Task<(IEnumerable<RecipeStep> recipeSteps, int totalCount)> GetRecipeStepsPaginatedAsync(int pageNumber, int pageSize);
         Task<RecipeStep> GetRecipeStepByIdAsync(int id);
         Task<(IEnumerable<RecipeStep> recipeSteps, int totalCount)> GetRecipeStepsByRecipeIdPaginatedAsync(int recipeId, int pageNumber, int pageSize);
+        Task AddRecipeStepsAsync(int recipeId, List<RecipeStep> steps);
+        Task UpdateRecipeStepAsync(int recipeId, RecipeStep step);
+        Task DeleteRecipeStepAsync(int recipeId, byte stepNum);
     }
 
 }
